5ed5fe0825cced2d0be155b97a1c2b2faacf033a
Vironika <nika.nika1711@gmail.com>
2022-11-17 17:24:35 +0400

    init vue auth

---
+++ vue-auth/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <sidebar />
+
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from "./views/sidebar.vue";
+</script>
